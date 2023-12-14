06225b221b6e57cbac50de0446a03b2e9ff8270d
jenhil0809 <jenhil0809@highgateschool.org.uk>
2023-12-14 13:12:50 +0000

    Created file for tkinter GUI

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rock Paper Scissors (Lizard Spock)
+This repository contains code for a rock paper scissors game.
+
+## Contents
+- Game object
+- CLI Interface
+- GUI Interface
+
+Find the rules for rock, paper, scissors, lizard, spock [here](https://upload.wikimedia.org/wikipedia/en/c/cc/Rock_paper_scissors_lizard_spock.png)
--- graphical_user_interface.py
+++ graphical_user_interface.py
@@ -5,3 +5,26 @@ import main
 class Game(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.title = tk.Label(text="Rock Paper Scissors game")
+        self.title.pack()
+        self.frame = SetUpFrame()
+        self.frame.pack()
+
+
+class SetUpFrame(tk.Frame):
+    def __init__(self):
+        super().__init__()
+        self.max_rounds_title = tk.Label(self, text="Maximum rounds:")
+        self.max_rounds = tk.Scale(self, from_=0, to=10, orient="horizontal")
+        self.place_widgets()
+        self.config(bg='yellow')
+
+    def place_widgets(self):
+        # self.max_rounds_title.grid(row=0, column=0)
+        # self.max_rounds.grid(row=0, column=1)
+        self.max_rounds_title.pack()
+
+
+if __name__ == "__main__":
+    app = Game()
+    app.mainloop()
