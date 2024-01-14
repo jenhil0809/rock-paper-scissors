10b9c9e7d4a4e75d9b7be96fb98fba4ae91bcd8b
jenhil0809 <jenhil0809@highgateschool.org.uk>
2024-01-14 15:56:58 +0000

    Seperated CLI from main program

---
+++ command_line_interface.py
@@ -0,0 +1,48 @@
+import main
+class CLInterface:
+    def __init__(self):
+        self.mode = main.inputMenu(["rps", "rpsls"], "rps or rpsls?\n")
+        self.game = main.Game("Game", self.mode)
+
+    def set_up(self):
+        for i in range(2):
+            typ = main.inputMenu(["computer", "human"], "Computer/human?\n")
+            if typ == "human":
+                name = input("Name: ")
+                self.game.add_human_player(name)
+            else:
+                self.game.add_computer_player()
+        self.set_max_rounds()
+
+    def set_max_rounds(self):
+        val = " "
+        while not val.isdecimal():
+            val = input("Maximum rounds: ")
+        self.game.max_rounds = val
+
+    def get_choices(self):
+        for player in self.game.players:
+            player.choose_object()
+
+    def run_game(self):
+        while not self.game.is_finished():
+            self.game.next_round()
+            self.get_choices()
+            self.game.find_winner()
+            self.game.report_round()
+            self.game.report_score()
+            print("------")
+        self.game.report_winner()
+
+    def run_sequence(self):
+        while True:
+            self.set_up()
+            while not (input("Do you want to play again?").lower()[0] == "n"):
+                self.run_game()
+                self.game.reset()
+            print("You have quit the game")
+
+
+if __name__ == "__main__":
+    cli_interface = CLInterface()
+    cli_interface.run_sequence()
